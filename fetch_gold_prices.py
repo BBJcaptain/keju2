4e4380a09fc54d1b5c5d1fa9635eda5bb96563b9
Captain <92660241+BBJcaptain@users.noreply.github.com>
2026-02-18 17:50:43 +0800

    Updated fetch_uob_prices function to use JSON API approach

--- fetch_gold_prices.py
+++ fetch_gold_prices.py
@@ -1,23 +1,19 @@
 def fetch_uob_prices():
     import requests
+    import json
 
-    response = requests.get('https://uob-api-endpoint')
+    url = 'https://api.example.com/prices'
+    response = requests.get(url)
     data = response.json()
 
     prices = {}
 
-    for item in data['types']:
-        if item['description'] == '1kg cast bar':
-            prices['1kg_cast_bar'] = {
-                'unit': item['unit'],
-                'bankBuy': item['bankBuy'],
-                'bankSell': item['bankSell']
-            }
-        elif item['description'] == '100g argor':
-            prices['100g_argor'] = {
-                'unit': item['unit'],
-                'bankBuy': item['bankBuy'],
-                'bankSell': item['bankSell']
+    for item in data.get('types', []):
+        description = item.get('description')
+        if description in ['ACB', 'CTB']:
+            prices[description] = {
+                'buy': item.get('bankBuy'),
+                'sell': item.get('bankSell')
             }
 
     return prices
